0d7bd48caa7cb2de8932822538b36230033dae6c
ibrahim anjum <ibanjum@gmail.com>
2020-05-11 11:35:30 -0500

    sec commit

---
+++ cvtandroid.Android/SplashScreen.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+
+
+namespace cvtandroid.Droid
+{
+    [Activity(Theme = "@style/splashscreen",
+              MainLauncher = true,
+              NoHistory = true)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
